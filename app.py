8e7b0c07ca9ced7d613075e49db4d0fed14430a8
Caleb Dame <54949405+calebdame@users.noreply.github.com>
2023-10-21 16:33:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -20,6 +20,20 @@ def client_ip():
     except:
         pass
 
+def get_user_agent():
+
+    script = ('navigator.userAgent')
+
+    try:
+        user_agent = st_javascript(script)
+        if user_agent:
+            return user_agent
+    except:
+        pass
+
+user_agent_str = get_user_agent()
+
+st.write(f"User-Agent: {user_agent_str}")  # Log the user-agent string
 
 ip_address = client_ip()  # now you have it in the host...
 
