73b82b6789fe4a669e11207797062eca35c61ef2
Caleb Dame <54949405+calebdame@users.noreply.github.com>
2023-10-21 16:24:42 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,26 @@
 import streamlit as st
-from streamlit import server
-from urllib.parse import parse_qs
-import requests
+from streamlit_javascript import st_javascript
 
-def log_info():
-    """Capture and log information."""
-    ctx = server.context
-    if ctx:
-        # Check if 'args' are available in the query string
-        args = parse_qs(ctx.request.query)
-        ip = args.get('ip', [''])[0]
-        user_agent = args.get('user_agent', [''])[0]
-        st.write(f"Logged IP: {ip}")
-        st.write(f"Logged User-Agent: {user_agent}")
 
-st.title('Capture and Log Client Info')
+def client_ip():
 
-# Add JavaScript to capture the client info
-st.markdown(
-    """
-    <script>
-        async function fetchInfo() {
-            // Fetch public IP
-            const ipResponse = await fetch('https://api.ipify.org?format=json');
-            const ipData = await ipResponse.json();
-            const ip = ipData.ip;
+    url = 'https://api.ipify.org?format=json'
 
-            // Get User Agent
-            const userAgent = navigator.userAgent;
+    script = (f'await fetch("{url}").then('
+                'function(response) {'
+                    'return response.json();'
+                '})')
 
-            // Construct the GET request URL
-            const url = `/log_info?ip=${ip}&user_agent=${encodeURIComponent(userAgent)}`;
+    try:
+        result = st_javascript(script)
 
-            // Send GET request to Streamlit backend
-            fetch(url);
-        }
-        fetchInfo();
-    </script>
-    """,
-    unsafe_allow_html=True,
-)
+        if isinstance(result, dict) and 'ip' in result:
+            return result['ip']
 
-# Log the info in Streamlit
-log_info()
+    except:
+        pass
 
-# Other Streamlit content
-st.write('This is a test page.')
+
+ip_address = client_ip()  # now you have it in the host...
+
+st.write(ip_address)  # so you can log it, etc.
