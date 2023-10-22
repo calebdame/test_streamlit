2e2bcca4c41c101357ebc1b14aea204576d1d5b8
Caleb Dame <54949405+calebdame@users.noreply.github.com>
2023-10-21 22:58:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from streamlit_javascript import st_javascript
 
+if len(st.session_state) == 0:
+    st.session_state["ip"] = []
+    st.session_state["uas"] = []
+    
 
 def client_ip():
 
@@ -32,9 +36,12 @@ def get_user_agent():
         pass
 
 user_agent_str = get_user_agent()
-
+st.session_state["uas"].append(user_agent_str)
 st.write(f"User-Agent: {user_agent_str}")  # Log the user-agent string
 
 ip_address = client_ip()  # now you have it in the host...
 
+st.session_state["ip"].append(ip_address)
 st.write(ip_address)  # so you can log it, etc.
+
+st.write(st.session_state)
